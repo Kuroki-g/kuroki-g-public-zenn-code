39f5275b06f4aca9090baf9e064b434c170edd18
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-03-08 23:45:41 +0900

    add sample code for check

--- articles/dotnet-duckdb-excel-read/Program.cs
+++ articles/dotnet-duckdb-excel-read/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DuckDB.NET.Data;
+
+using var duckDBConnection = new DuckDBConnection("Data Source=./data/file.db");
+duckDBConnection.Open();
+
+using var command = duckDBConnection.CreateCommand();
+
+command.CommandText = "CREATE TABLE integers(foo INTEGER, bar INTEGER);";
+var executeNonQuery = command.ExecuteNonQuery();
+
+command.CommandText = "INSERT INTO integers VALUES (3, 4), (5, 6), (7, 8);";
+executeNonQuery = command.ExecuteNonQuery();
+
+command.CommandText = "Select count(*) from integers";
+var executeScalar = command.ExecuteScalar();
+
+command.CommandText = "SELECT foo, bar FROM integers";
+var reader = command.ExecuteReader();
+
+PrintQueryResults(reader);
+
+static void PrintQueryResults(DuckDBDataReader queryResult)
+{
+  for (var index = 0; index < queryResult.FieldCount; index++)
+  {
+    var column = queryResult.GetName(index);
+    Console.Write($"{column} ");
+  }
+
+  Console.WriteLine();
+
+  while (queryResult.Read())
+  {
+    for (int ordinal = 0; ordinal < queryResult.FieldCount; ordinal++)
+    {
+      var val = queryResult.GetInt32(ordinal);
+      Console.Write(val);
+      Console.Write(" ");
+    }
+
+    Console.WriteLine();
+  }
+}
