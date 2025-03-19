52cf685e76f696b335a47508a2f76ad7e0f32d27
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-03-19 22:29:58 +0900

    add get api codes

--- articles/fsharp-min-web-api/TodoApi/src/App/Program.fs
+++ articles/fsharp-min-web-api/TodoApi/src/App/Program.fs
@@ -1,9 +1,13 @@
 open System
+open System.Linq
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.EntityFrameworkCore
+open System.Threading.Tasks
 open TodoApi.Data
+open TodoApi.Models
 
 [<EntryPoint>]
 let main args =
@@ -16,7 +20,26 @@ let main args =
 
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/todoitems", Func<TodoDb, Task<Collections.Generic.List<Todo>>>(fun db -> db.Todos.ToListAsync()))
+    |> ignore
+
+    app.MapGet(
+        "/todoitems/complete",
+        Func<TodoDb, Task<Collections.Generic.List<Todo>>>(fun db ->
+            db.Todos.Where(fun t -> t.IsComplete).ToListAsync())
+    )
+    |> ignore
+
+    app.MapGet(
+        "/todoitems/{id}",
+        Func<int, TodoDb, Task<IResult>>(fun id db ->
+            task {
+                match! db.Todos.FindAsync id with
+                | null -> return Results.NotFound()
+                | todo -> return Results.Ok todo
+            })
+    )
+    |> ignore
 
     app.Run()
 
