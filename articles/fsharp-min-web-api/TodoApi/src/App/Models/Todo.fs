602aac6b25f5a7a0cf8a1d5e235c1822fe1cd452
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-03-20 17:10:45 +0900

    change model from record to class

--- articles/fsharp-min-web-api/TodoApi/src/App/Models/Todo.fs
+++ articles/fsharp-min-web-api/TodoApi/src/App/Models/Todo.fs
@@ -1,7 +1,6 @@
 namespace TodoApi.Models
 
-[<CLIMutableAttribute>]
-type Todo =
-    { Id: int
-      mutable Name: string | null
-      mutable IsComplete: bool }
+type Todo() =
+    member val Id: int = 0 with get, set
+    member val Name: string | null = null with get, set
+    member val IsComplete: bool = false with get, set
